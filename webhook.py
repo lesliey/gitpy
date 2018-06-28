9dcfa24ca01f843025d38c475533cfd2436d86e1
杨海校 <yanghaixiao@hd123.com>
2018-06-28 11:00:56 +0800

    init

--- webhook.py
+++ webhook.py
@@ -1,7 +1,14 @@
 import os
+import json
 def application(environ, start_response):
     start_response('200 OK', [('Content-Type', 'text/html')])
-    os.system('cd /usr/share/nginx/www/myblog && git pull origin master')
-    os.system('cd /home/gitpy && git pull origin master')
-    print('git pull finish')
-    return [b'Hello, webhook!']
+    path = environ['PATH_INFO']
+    if (path == '/hook'):
+        os.system('cd /usr/share/nginx/www/myblog && git pull origin master')
+        os.system('cd /home/gitpy && git pull origin master')
+        print('git pull finish')
+        return [b'Hello, webhook has done!']
+    if (path == '/hello'):
+        print('hello..')
+        return [b'Hello, just welcome']
+    return [b'Hello, default!']
